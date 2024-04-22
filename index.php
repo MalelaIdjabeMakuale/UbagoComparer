b18301121ac75ddf49ccec25d1958ca939d264aa
bokombe <malela.im90@gmail.com>
2024-04-22 17:10:33 +0200

    arreglado error estilos

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ubago comparer</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../AlexUbagoComparer/styles.css">
 </head>
 <body class="body ">
   
